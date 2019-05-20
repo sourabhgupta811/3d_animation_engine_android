1c833d907da3dae6f928448d6b58df3c22e9ebc7
sourabhgupta811 <sourabh@totalitycorp.com>
2019-05-20 19:15:58 +0530

    initial commit

--- app/src/main/java/com/astro/rajawalisample/MainActivity.java
+++ app/src/main/java/com/astro/rajawalisample/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-import com.astro.rajawalisample.R;
 
 import org.rajawali3d.materials.RajawaliContext;
 
@@ -20,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animation);
         RajawaliContext.getInstance().setContext(this);
-//        getSupportFragmentManager().beginTransaction().add(R.id.frame_view4, AnimationFragment.newInstance("name3")).commit();
+        getSupportFragmentManager().beginTransaction().add(R.id.frame_view4, AnimationFragment.newInstance("name3")).commit();
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -28,17 +27,17 @@ public class MainActivity extends AppCompatActivity {
                 getSupportFragmentManager().beginTransaction().add(R.id.frame_view2, AlphaFragment.newInstance("name1")).commit();
             }
         }, 200);
-//        new Handler().postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                getSupportFragmentManager().beginTransaction().add(R.id.frame_view3, AlphaFragment.newInstance("name2")).commit();
-//            }
-//        }, 400);
-//        new Handler().postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                getSupportFragmentManager().beginTransaction().add(R.id.frame_view, AlphaFragment.newInstance("name")).commit();
-//            }
-//        }, 600);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                getSupportFragmentManager().beginTransaction().add(R.id.frame_view3, AlphaFragment.newInstance("name2")).commit();
+            }
+        }, 400);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                getSupportFragmentManager().beginTransaction().add(R.id.frame_view, AlphaFragment.newInstance("name")).commit();
+            }
+        }, 600);
     }
 }
