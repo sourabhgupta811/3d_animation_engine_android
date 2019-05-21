017a2dec555debb259dc7e41c131828719b2f820
sourabhgupta811 <sourabh@totalitycorp.com>
2019-05-21 13:05:26 +0530

    initial commit

--- app/src/main/java/com/astro/rajawalisample/AlphaFragment.java
+++ app/src/main/java/com/astro/rajawalisample/AlphaFragment.java
@@ -224,4 +224,77 @@ public class AlphaFragment extends Fragment {
             super.onRenderFrame(gl);
         }
     }
+    private class PlaneRenderer extends Renderer{
+        @Override
+        protected void initScene() {
+            DirectionalLight light = new DirectionalLight(0, 0, 1);
+
+            getCurrentScene().addLight(light);
+            getCurrentCamera().setPosition(0, 0, -16);
+
+            final PlanesGalore planes = new PlanesGalore();
+            mMaterial = planes.getMaterial();
+            mMaterial.setColorInfluence(0);
+            try {
+                mMaterial.addTexture(new Texture("flickrPics", R.drawable.flickrpics));
+            } catch (ATexture.TextureException e) {
+                e.printStackTrace();
+            }
+
+            mMaterialPlugin = planes.getMaterialPlugin();
+
+            planes.setDoubleSided(true);
+            planes.setZ(4);
+            getCurrentScene().addChild(planes);
+
+            Object3D empty = new Object3D();
+            getCurrentScene().addChild(empty);
+
+            CatmullRomCurve3D path = new CatmullRomCurve3D();
+            path.addPoint(new Vector3(-4, 0, -20));
+            path.addPoint(new Vector3(2, 1, -10));
+            path.addPoint(new Vector3(-2, 0, 10));
+            path.addPoint(new Vector3(0, -4, 20));
+            path.addPoint(new Vector3(5, 10, 30));
+            path.addPoint(new Vector3(-2, 5, 40));
+            path.addPoint(new Vector3(3, -1, 60));
+            path.addPoint(new Vector3(5, -1, 70));
+
+            final SplineTranslateAnimation3D anim = new SplineTranslateAnimation3D(path);
+            anim.setDurationMilliseconds(20000);
+            anim.setRepeatMode(Animation.RepeatMode.REVERSE_INFINITE);
+            anim.setTransformable3D(getCurrentCamera());
+            anim.setInterpolator(new AccelerateDecelerateInterpolator());
+            getCurrentScene().registerAnimation(anim);
+            anim.play();
+
+            getCurrentCamera().setLookAt(new Vector3(0, 0, 30));
+        }
+
+        @Override
+        public void onRenderSurfaceCreated(EGLConfig config, GL10 gl, int width, int height) {
+            super.onRenderSurfaceCreated(config, gl, width, height);
+            mStartTime = System.currentTimeMillis();
+        }
+
+        @Override
+        protected void onRender(long elapsedRealtime, double deltaTime) {
+            super.onRender(elapsedRealtime, deltaTime);
+            mMaterial.setTime((System.currentTimeMillis() - mStartTime) / 1000f);
+            mMaterialPlugin.setCameraPosition(getCurrentCamera().getPosition());
+        }
+        public PlaneRenderer(Context context) {
+            super(context);
+        }
+
+        @Override
+        public void onOffsetsChanged(float xOffset, float yOffset, float xOffsetStep, float yOffsetStep, int xPixelOffset, int yPixelOffset) {
+
+        }
+
+        @Override
+        public void onTouchEvent(MotionEvent event) {
+
+        }
+    }
 }
